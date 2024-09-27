6f9d56dcd938b3a9ce2b9174802c651272b3770f
Golan Shay <golanshy@gmail.com>
2024-09-27 13:41:34 +0100

    wip

---
+++ grpc_client/main/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	pb "github.com/golanshy/grpc-playground/gen"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+//goland:noinspection ALL
+func main() {
+
+	log.Print("Setting up GRPC Client")
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Second))
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewKeyValueClient(conn)
+
+	var action, key, value string
+
+	if len(os.Args) > 2 {
+		action, key = os.Args[1], os.Args[2]
+		value = strings.Join(os.Args[3:], " ")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	// Call client.Get() or client.Put()
+
+	switch action {
+
+	case "get":
+		r, err := client.Get(ctx, &pb.GetRequest{Key: key})
+		if err != nil {
+			log.Fatalf("Could not get value for key %s: %v", key, err)
+		}
+		log.Printf("Get %s returns %s", key, r.Value)
+
+	case "put":
+		_, err := client.Put(ctx, &pb.PutRequest{Key: key, Value: value})
+		if err != nil {
+			log.Fatalf("Could not get value for key %s: %v", key, err)
+		}
+		log.Printf("Put %s %s", key, value)
+
+	default:
+		log.Fatalf("Could not get value for key %s: not implemented", key)
+	}
+}
